fab679a34c8c22f443beb2f97fa528137e72eaeb
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-03-10 09:40:28 -0700

    fixed 1pass auth

--- app1.py
+++ app1.py
@@ -4,14 +4,26 @@ from passageidentity import Passage, PassageError
 from flask import Flask, request, jsonify, render_template_string
 import pandas as pd
 import joblib
+from main import auth
 
 import os
 
-PASSAGE_APP_ID = os.environ.get("PASSAGE_APP_ID")
-PASSAGE_API_KEY = os.environ.get("PASSAGE_API_KEY")
+PASSAGE_APP_ID = 'v2jbtAXbi7gF6tIDH0oMvBw0'
+PASSAGE_API_KEY ='jndJqbt0NE.cnF50aSCBepyMNbOqhog01bgXOaH5EhuEIcyaK5jR0hwonWVIFJiyPCEuM2Hk9Yw'
+try:
+    psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
+except PassageError as e:
+    print(e)
+    exit()
 
-
-
+# decorator that will run before every route in the auth blueprint
+@auth.before_request
+def before_request():
+    try:
+        g.user = psg.authenticateRequest(request)
+    except PassageError as e:
+        # this is an issue with the auth check, return 401
+        return render_template('unauthorized.html')
 
 
 app = Flask(__name__)
@@ -122,6 +134,15 @@ def index():
     """
     
     return form_html
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    # g.user will be set here. 
+	  # use Passage to get the user information and add it to the dashboard
+    psg_user = psg.getUser(g.user)
+
+    return render_template('dashboard.html', email=psg_user.email)
 
 @app.route('/predict', methods=["POST"])
 def ovulation_prediction():
